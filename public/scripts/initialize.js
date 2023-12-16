ce8526fad907617b7fa37bf60263b1668b4f4f57
kikeda1102 <kikeda1102@gmail.com>
2023-12-16 19:17:42 +0900

    検索機能

--- public/scripts/initialize.js
+++ public/scripts/initialize.js
@@ -9,15 +9,13 @@ async function initialize() {
         const clips = window.clips;
 
         // モーダルウィンドウを表示 userGestureを得るため
-        await showLoadModal(clips);
+        showLoadModal(clips);
 
         // isMonophonicの初期値を読み取り、ボタンのスタイルに反映
         const monophonicButton = document.getElementById('monophonicButton');
         const polyphonicButton = document.getElementById('polyphonicButton');
-
         monophonicButton.classList.toggle('btn-unselected', !isMonophonic);
         monophonicButton.classList.toggle('btn-selected', isMonophonic);
-
         polyphonicButton.classList.toggle('btn-unselected', isMonophonic);
         polyphonicButton.classList.toggle('btn-selected', !isMonophonic);
 
@@ -37,11 +35,11 @@ async function initialize() {
         //   closeNav();
         // });
 
-        // フィルター機能
+        // タグフィルター機能
         // clipsからwhoSaidのユニークな値を取得
         const uniqueWhoSaidValues = [...new Set(clips.map(clip => clip.whoSaid))];
 
-        // タグ用ボタンの生成
+        // タグフィルターのボタンの生成
         const tagButtonsContainer = document.querySelector(".accordion-section-content");
         uniqueWhoSaidValues.forEach(tag => {
             const button = document.createElement("button");
@@ -51,33 +49,60 @@ async function initialize() {
             tagButtonsContainer.appendChild(button);
         });
 
-        // 検索
+        // 検索フォームの生成
+        const searchFormContainer = document.querySelector(".search-form-content");
+        const searchForm = document.createElement("form");
+        searchForm.id = "searchForm";
+        // form要素にsubmitイベントリスナーを追加
+        searchForm.addEventListener('submit', function (event) {
+            event.preventDefault(); // デフォルトのsubmit動作をキャンセル
+            handleSearch(); // 検索処理を実行
+        });
+        const searchInput = document.createElement("input");
+        searchInput.type = "text";
+        searchInput.id = "searchInput";
+        searchInput.placeholder = "検索...";
+        searchInput.oninput = handleSearch;
+        searchForm.appendChild(searchInput);
+        searchFormContainer.appendChild(searchForm);
+
+
+
+        // ワード検索機能, 再生ボタンの生成
         function handleSearch() {
             const searchQuery = document.getElementById('searchInput').value.trim();
-            const searchResultsContainer = document.getElementById('searchResults');
+            const playButtonArea = document.getElementById('playButtonArea');
 
             // ここで検索処理を実装
             const searchResults = performSearch(searchQuery, clips);
 
             // 検索結果表示エリアをクリア
-            searchResultsContainer.innerHTML = '';
+            playButtonArea.innerHTML = '';
 
-            // 検索結果を表示
-            searchResults.forEach((clip, index) => {
-                const button = document.createElement("button");
-                button.type = "button";
-                button.textContent = clip.title;
-                button.onclick = () => {
-                    // 単音モードの場合、選択された音のみ再生
-                    if (isMonophonic) {
-                        playMonoAudio(audioBuffers[index]);
-                    } else {
-                        // 複音モードの場合、すべての音を同時に再生
-                        playPolyAudio(audioBuffers[index]);
-                    }
-                };
-                document.body.appendChild(button);
-            });
+            // 検索結果があるかどうかをチェック
+            if (searchResults.length > 0) {
+                // 検索結果がある場合はボタンを表示
+                searchResults.forEach((clip, index) => {
+                    const button = document.createElement("button");
+                    button.type = "button";
+                    button.textContent = clip.title;
+                    button.onclick = () => {
+                        // 単音モードの場合、選択された音のみ再生
+                        if (isMonophonic) {
+                            playMonoAudio(audioBuffers[index]);
+                        } else {
+                            // 複音モードの場合、すべての音を同時に再生
+                            playPolyAudio(audioBuffers[index]);
+                        }
+                    };
+                    playButtonArea.appendChild(button);
+                });
+            } else {
+                // 検索結果がない場合はメッセージを表示
+                const noResultsMessage = document.createElement("p");
+                noResultsMessage.textContent = "検索結果がありません";
+                playButtonArea.appendChild(noResultsMessage);
+            }
         }
 
         function performSearch(query, clips) {
@@ -97,24 +122,6 @@ async function initialize() {
             });
             return searchResults;
         }
-
-
-        // 検索フォームの生成
-        const searchFormContainer = document.querySelector(".search-form-content");
-        const searchForm = document.createElement("form");
-        searchForm.id = "searchForm";
-        // form要素にsubmitイベントリスナーを追加
-        searchForm.addEventListener('submit', function (event) {
-            event.preventDefault(); // デフォルトのsubmit動作をキャンセル
-            handleSearch(); // 検索処理を実行
-        });
-        const searchInput = document.createElement("input");
-        searchInput.type = "text";
-        searchInput.id = "searchInput";
-        searchInput.placeholder = "検索...";
-        searchInput.oninput = "handleSearch()";
-        searchForm.appendChild(searchInput);
-        searchFormContainer.appendChild(searchForm);
 
 
     } catch (error) {
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,7 +1,31 @@
+// プリロードされた audioBuffers を取得する関数
+async function preloadAudioBuffers(clips, progressBar) {
+    // Web Audio APIコンテキストの作成
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    // 音声の preload
+    const audioBuffers = await preloadAllAudio(clips, progressBar);
+
+    // ブラウザの再生状態を確認
+    if (audioContext.state === "suspended") {
+        // AudioContext が一時停止状態の場合、ユーザージェスチャー後に再開
+        document.addEventListener(
+            "click",
+            function () {
+                audioContext.resume().then(() => {
+                    console.log("AudioContext resumed successfully");
+                });
+            },
+            { once: true }
+        );
+    }
+
+    return audioBuffers;
+}
+
 // 確認ダイアログを表示する関数
 async function showLoadModal(clips) {
     return new Promise(async (resolve) => {
-
         // モーダルを表示
         document.getElementById('loadModal').style.display = 'block';
 
@@ -18,27 +42,10 @@ async function showLoadModal(clips) {
         document.getElementById('startLoading').addEventListener('click', async function () {
             this.classList.add('clicked');
             try {
+                // audioBuffersの取得
+                const audioBuffers = await preloadAudioBuffers(clips, progressBar);
 
-                // Web Audio APIコンテキストの作成
-                audioContext = new (window.AudioContext || window.webkitAudioContext)();
-                // preload
-                const audioBuffers = await preloadAllAudio(clips, progressBar);
-
-                // ブラウザの再生状態を確認
-                if (audioContext.state === "suspended") {
-                    // AudioContextが一時停止状態の場合、ユーザージェスチャー後に再開
-                    document.addEventListener(
-                        "click",
-                        function () {
-                            audioContext.resume().then(() => {
-                                console.log("AudioContext resumed successfully");
-                            });
-                        },
-                        { once: true }
-                    );
-                }
-
-                // playボタンの作成とクリック時の処理を設定
+                // 音声再生ボタンの生成
                 clips.forEach((clip, index) => {
                     const button = document.createElement("button");
                     button.type = "button";
@@ -52,7 +59,7 @@ async function showLoadModal(clips) {
                             playPolyAudio(audioBuffers[index]);
                         }
                     };
-                    document.body.appendChild(button);
+                    playButtonArea.appendChild(button);
                 });
 
                 // プリロードが完了したら Modal を閉じる
